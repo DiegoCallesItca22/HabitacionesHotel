780e0b6d9222631b279516c01d702148a7a06a36
Pedr0V1ll4lob0s <pedro.villalobos22@itca.edu.sv>
2026-05-03 20:44:25 -0600

    Agregar migraciones de la base de datos

---
+++ database/migrations/2024_01_01_000005_crear_tabla_servicios.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Ejecutar las migraciones.
+     */
+    public function up(): void
+    {
+        Schema::create('servicios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100);
+            $table->decimal('precio', 10, 2);
+            $table->boolean('activo')->default(true);
+            
+            $table->index('nombre');
+            $table->index('activo');
+        });
+    }
+
+    /**
+     * Revertir las migraciones.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicios');
+    }
+};
