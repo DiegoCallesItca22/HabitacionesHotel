d6645a9d82c155f95e0ade914dd29aaaaff5c761
Pedr0V1ll4lob0s <pedro.villalobos22@itca.edu.sv>
2026-05-05 17:49:42 -0600

    Agregar modelo Usuario

---
+++ app/Models/Usuario.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Usuario extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'usuarios';
+    
+    const CREATED_AT = 'creado_en';
+    const UPDATED_AT = 'actualizado_en';
+
+    protected $fillable = [
+        'nombre',
+        'correo',
+        'password',
+        'rol',
+        'activo'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'activo' => 'boolean',
+        'creado_en' => 'datetime',
+        'actualizado_en' => 'datetime',
+    ];
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class, 'usuario_id');
+    }
+
+    public function getAuthIdentifierName()
+    {
+        return 'correo';
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+}
